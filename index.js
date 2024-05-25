efb76ca1f8175cab5362f62787490e481c5b7f55
Tayyab88884 <162956028+Tayyab88884@users.noreply.github.com>
2024-05-25 13:02:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome to Code With Muhammad Naeem Akhtar Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit From 1 to 5):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations you guess a correct number");
+}
+else {
+    console.log("Sorry , you guess a wrong number");
+}
